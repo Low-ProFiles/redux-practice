ae2859b281b9ab3a32ce82c128bca8411afa9200
Low-profile <maxandy@naver.com>
2022-06-10 11:45:39 +0900

    feat: making todolist with react-redux

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Detail = () => {
+const Detail = ({ toDo }) => {
     return (
-        <div>
-            Hi;
-        </div>
+        <>
+            <h1>
+                {toDo?.text}
+            </h1>
+            <h5>Created at : {toDo?.id}</h5>
+        </>
     )
 }
 
-export default Detail;
+function mapStateToProps(state, ownProps) {
+    const { match: { params: { id } } } = ownProps;
+    return { toDo: state.find(toDo => toDo.id === parseInt(id)) }
+}
+
+export default connect(mapStateToProps)(Detail);
